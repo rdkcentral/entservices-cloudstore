ad6495786385d750f71824fc64079762a539ddbf
Simon Chung <simon.chung@rdkcentral.com>
2026-04-17 14:53:34 +0100

    Fix LOGINFO build error

--- cov_build.sh
+++ cov_build.sh
@@ -70,6 +70,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-cloudstore \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/DobbyProxy.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/Public/Dobby/IDobbyProxy.h \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/Dobby/IpcService/IpcFactory.h \
+-include ${GITHUB_WORKSPACE}/helpers \
 -Wall -Werror -Wno-error=format \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
 -DENABLE_TELEMETRY_LOGGING -DHAS_API_SYSTEM \
